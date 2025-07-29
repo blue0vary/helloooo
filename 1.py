e1b5d4efe1f9e95d13a55b990cc1c0a6921207fe
blue0vary <blue0vary@gmail.com>
2025-07-29 12:13:48 +0900

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,19 +1,22 @@
 import streamlit as st
-import altair as alt
-import pandas as pd
-import numpy as np
-st.header('st.write')
-st.write('Hello,*World!*:sunglasses')
-st.write(1234)
-df=pd.DataFrame({
-    '첫번째 칼럼':[1,2,3,4],
-    '두번째 칼럼':[10,20,30,40]
-    })
-st.write(df)
-st.write('아래는 DataFrame입니다.',df,'위는 dataframe입니다.')
-df2=pd.DataFrame(
-    np.random.randn(200,3),
-    columns=['a','b','c'])
-c=alt.Chart(df2).mark_circle().encode(
-    x='a',y='b',color='c',tooltip=['a','b','c'])
-st.write(c)
+from datetime import time,datetime
+st.header('st.slider')
+st.subheader('Slider')
+age=st.slider('당신의 나이는?',0,130,25)
+st.write("나는",age,'살 입니다.')
+st.subheader('범위 슬라이더')
+values=st.slider(
+    '값의 범위를 선택하세요',
+    0.0,100.0,(25.0,75.0))
+st.write('값:',values)
+st.subheader('시간 범위 슬라이더')
+appointment=st.slider(
+    "약속을 예약하세요",
+    value=(time(11,30),time(12,45)))
+st.write("예약된 시간:",appointment)
+st.subheader('날짜 및 시간 슬라이더')
+start_time=st.slider(
+    "언제 시작하시겠습니까?",
+    value=datetime(2020,1,1,9,30),
+    format="MM/DD/YY-hh:mm")
+st.write("시작 시간:", start_time)
